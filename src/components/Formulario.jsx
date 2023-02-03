37c50c67716d3b131a30f8f254490fdcacfa8815
agustintrossero <agustintrossero@gmail.com>
2023-02-03 20:15:26 +0100

    consultamos una API externa con async await y fetch, generamos a partir de la API y su respuesta en json() un array con map para filtrar informacion, con el state del hook setCriptos le asignamos el array consumido por la API

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import {useState, useEffect} from 'react'
 import styled from '@emotion/styled'
 import useSelectMonedas from '../hooks/useSelectMonedas'
 import {monedas} from '../data/monedas'
@@ -23,12 +23,37 @@ const InputSubmit = styled.input`
 
 const Formulario = () => {
 
+    const [criptos, setCriptos] = useState([])
+
     const [moneda, SelectMonedas] = useSelectMonedas("Elige tu Moneda", monedas)    
+
+    useEffect(() => {
+        const consultarAPI = async (req, res) => {
+            const url = "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD"
+            const respuesta = await fetch(url)
+            //console.log(respuesta)
+            const resultado = await respuesta.json()
+            //console.log(resultado.Data)
+
+            const arrayCriptos = resultado.Data.map(cripto => {
+                //console.log(cripto.ConiInfo)
+                const objeto ={
+                    id: cripto.CoinInfo.Name,
+                    nombre: cripto.CoinInfo.FullName
+                }
+                //console.log(objeto)
+                return objeto
+            })
+            //console.log(arrayCriptos)
+            setCriptos(arrayCriptos)
+        }
+        consultarAPI();
+    },[])
 
   return (
     <form>
         <SelectMonedas/>
-        
+
         <InputSubmit 
             type="submit" 
             value="Cotizar" 
