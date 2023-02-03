2cbea27e081b01a85ca855c5f139523492eae07b
agustintrossero <agustintrossero@gmail.com>
2023-02-03 18:59:41 +0100

    creacion del componete Select con styled  al que se le hace map de las opciones que las lee asignadas en el hook de usSelectMondeas que como parametro lee el array de monedas desde data/monedas.js, tambien se agregaron los estilos del label con styled

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from '@emotion/styled'
 import useSelectMonedas from '../hooks/useSelectMonedas'
+import {monedas} from '../data/monedas'
 
 const InputSubmit = styled.input`
     background-color: #9497ff;
@@ -13,6 +14,7 @@ const InputSubmit = styled.input`
     font-size: 20px;
     border-radius: 5px;
     transition: background-color .3s ease;
+    margin-top: 30px;
     &:hover{
         background-color: #7a7dfe;
         cursor: pointer;
@@ -21,7 +23,7 @@ const InputSubmit = styled.input`
 
 const Formulario = () => {
 
-    const [SelectMonedas] = useSelectMonedas("Elige tu Moneda")    
+    const [SelectMonedas] = useSelectMonedas("Elige tu Moneda", monedas)    
 
   return (
     <form>
