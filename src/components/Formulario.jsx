0b5231132bc1b0f2791a3a77da5fd32c8a5abf83
agustintrossero <agustintrossero@gmail.com>
2023-02-03 19:17:02 +0100

    se le agrego a el hook useSelectMonedas state de manera generica para poder ser reutilizado, proximo paso hacer el select de crypto consumiendo una API externa

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -23,11 +23,12 @@ const InputSubmit = styled.input`
 
 const Formulario = () => {
 
-    const [SelectMonedas] = useSelectMonedas("Elige tu Moneda", monedas)    
+    const [moneda, SelectMonedas] = useSelectMonedas("Elige tu Moneda", monedas)    
 
   return (
     <form>
         <SelectMonedas/>
+        
         <InputSubmit 
             type="submit" 
             value="Cotizar" 
